ce312fbc6ed8b23c40a58e402bd07afbf1eaace6
Justin Yang <linuxjustin@gmail.com>
2024-02-24 23:05:53 +0800

    Add ipv6 nat support

--- config_nat.sh
+++ config_nat.sh
@@ -6,3 +6,7 @@ WAN_DEV="eth2"
 iptables -t nat -A POSTROUTING -o $WAN_DEV -j MASQUERADE
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $LAN_DEV -o $WAN_DEV -j ACCEPT
+
+ip6tables -t nat -A POSTROUTING -o $WAN_DEV -j MASQUERADE
+ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A FORWARD -i $LAN_DEV -o $WAN_DEV -j ACCEPT
