a37c4ac66ce61e4f1215854557532b72f58afcf4
Justin Yang <linuxjustin@gmail.com>
2024-02-26 21:37:55 +0800

    Use nft as it's more modern

--- config_nat.sh
+++ config_nat.sh
@@ -3,10 +3,10 @@
 LAN_DEV="br-lan"
 WAN_DEV="eth2"
 
-iptables -t nat -A POSTROUTING -o $WAN_DEV -j MASQUERADE
-iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $LAN_DEV -o $WAN_DEV -j ACCEPT
+nft add table inet nat
+nft add chain inet nat postrouting '{ type nat hook postrouting priority 100 ; }'
 
-ip6tables -t nat -A POSTROUTING -o $WAN_DEV -j MASQUERADE
-ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-ip6tables -A FORWARD -i $LAN_DEV -o $WAN_DEV -j ACCEPT
+nft add rule inet nat postrouting oifname $WAN_DEV masquerade
+nft add rule inet filter forward ct state related,established accept
+nft add rule inet filter forward iifname $LAN_DEV oifname $WAN_DEV accept
+
