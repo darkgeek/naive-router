3be63e7450beed6165d31008a7fa47ee0fedc420
Justin Yang <linuxjustin@gmail.com>
2024-03-03 17:35:07 +0800

    We use dhcpcd to retrieve ipv4/ipv6 for WAN interface, and do dhcpv6-pd for LAN interface. And we also use dhcpcd to assign static ipv4 address to br-lan, so no need to assing address in our script now

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ CURRENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 echo Step 1: init bridge
 $CURRENT_DIR/config_bridge.sh -a create
 
-echo Step 2: assign ip address to lan
-$CURRENT_DIR/config_lan_address.sh
+#echo Step 2: assign ip address to lan
+#$CURRENT_DIR/config_lan_address.sh
 
 echo Step 3: create NAT
 $CURRENT_DIR/config_nat.sh
