a4716173c49b41102cd2dec06536bf858efe9d64
Justin Yang <linuxjustin@gmail.com>
2024-03-05 23:51:47 +0800

    We transfer our nft settings to config file, reduce the script size. Though tcp offload related nft rules have to be added in script for now.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,5 @@ CURRENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 echo Step 1: init bridge
 $CURRENT_DIR/config_bridge.sh -a create
 
-echo Step 2: create NAT
-$CURRENT_DIR/config_nat.sh
-
-echo Step 3: Setting up flowtables fastpath
+echo Step 2: Setting up flowtables fastpath
 $CURRENT_DIR/enable_flowtables_fastpath.sh
