8bb993dff87402f6907525bc99ff5b98fce74e91
Justin Yang <linuxjustin@gmail.com>
2024-03-08 23:30:49 +0800

    dhcpcd-run-hooks is a good thing, we use that to initialize bridge device and add some nft rules there. As a result, we get rid of our home-made router-setup.service!

--- enable_flowtables_fastpath.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# See: https://thermalcircle.de/doku.php?id=blog:linux:flowtables_1_a_netfilter_nftables_fastpath
-
-nft add table inet filter
-nft add flowtable inet filter f \
-   { hook ingress priority 0\; devices = { br-lan, eth2 }\; }
-nft add chain inet filter forward { type filter hook forward priority 0\; }
-nft add rule inet filter forward meta l4proto tcp flow add @f
--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CURRENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-
-echo Step 1: init bridge
-$CURRENT_DIR/config_bridge.sh -a create
-
-echo Step 2: Setting up flowtables fastpath
-$CURRENT_DIR/enable_flowtables_fastpath.sh
